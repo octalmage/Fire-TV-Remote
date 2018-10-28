73d59b8c23143c29a79c0b13861462e66321ea04
Jason Stallings <jason@stallin.gs>
2018-10-28 00:50:15 -0500

    Initial Commit

---
+++ Fire TV Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fire TV Remote
+//
+//  Created by Jason Stallings on 10/28/18.
+//  Copyright © 2018 Jason Stallings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
